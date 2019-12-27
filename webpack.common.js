b6df3c1c17db3dd6f89f07e82926e5e43d6e2b12
rathod-sahaab <abhayonlyone@gmail.com>
2019-12-28 00:00:47 +0530

    renamed references to lib

--- webpack.common.js
+++ webpack.common.js
@@ -1,41 +1,36 @@
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry : {
-    app : './src/scripts/app.js',
-    vendor : './src/scripts/vendor.js',
+  entry: {
+    app: './src/scripts/app.js',
+    vendor: './src/scripts/vendor.js',
   },
-  plugins : [
-    new CopyPlugin([
-      {from : './src/root/', to : './'},
-    ]),
-  ],
-  target : 'node',
-  module : {
-    rules : [
+  plugins: [new CopyPlugin([{ from: './src/root/', to: './' }])],
+  target: 'web',
+  module: {
+    rules: [
       {
-        test : /\.js$/,
-        exclude : /node_modules/,
-        use : {
-          loader : 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
       },
       {
-        test : /\.pug$/,
-        use : [ 'html-loader', 'pug-html-loader' ],
+        test: /\.pug$/,
+        use: ['html-loader', 'pug-html-loader'],
       },
       {
-        test : /\.(svg|png|jpg|gif)/,
-        use : {
-          loader : 'file-loader',
-          options : {
-            name : '[name].[hash].[ext]',
-            outputPath : 'imgs',
-            esModule : false,
+        test: /\.(svg|png|jpg|gif)/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs',
+            esModule: false,
           },
         },
-
       },
     ],
-  }
+  },
 };
